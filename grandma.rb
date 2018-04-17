c8ff33efac25c64ce610f416e19107f37cab3222
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-17 01:47:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!""HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+    
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "
   else
