4f81f27b3597f3447fb39ce0c62a9c31e09e9def
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-17 01:39:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
-  if phrase.downcase
+  if !phrase.upcase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.upcase "I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
